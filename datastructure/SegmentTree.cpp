14a5799605763e3af4fd733c6c7dd4e0a09cd168
JP3BGY <6635381+JP3BGY@users.noreply.github.com>
2019-12-25 09:41:24 +0900

    modify

--- datastructure/SegmentTree.cpp
+++ datastructure/SegmentTree.cpp
@@ -69,7 +69,7 @@ class segtree2d{
     T unit;
     vector<vector<T>> tree;
     public:
-    segtree(F f,T unit,const vector<vector<T>> &v):f(f),unit(unit),tree(){
+    segtree2d(F f,T unit,const vector<vector<T>> &v):f(f),unit(unit),tree(){
         this->n=1;
         this->m=1;
         while(n<v.size())this->n<<=1;
@@ -77,7 +77,7 @@ class segtree2d{
         tree.assign(n,vector<T>(m,unit));
         build(v);
     }
-    segtree(F f,T unit,size_t n_):segtree(f,unit,vector<T>(n_,unit)){
+    segtree2d(F f,T unit,size_t n_):segtree(f,unit,vector<T>(n_,unit)){
     }
     void build(const vector<vector<T>> &v){
         for(size_t i=0;i<v.size();++i)tree[n+i]=build_segtree(f,unit,v[i]);
