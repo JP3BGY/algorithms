d461eca49938b4e855dec752dccee301f30ad61a
JP3BGY <6635381+JP3BGY@users.noreply.github.com>
2020-08-11 12:48:55 +0900

    speedup flow

--- datastructure/flow.cpp
+++ datastructure/flow.cpp
@@ -123,6 +123,62 @@ std::tuple<bool,long long> minCostFlow(ref_flow& f,size_t s,size_t t,long long c
         ret+=(potential[t])*tcap;
         auto curidx=t;
         while(curidx!=s){
+            auto [curcost,__,curfrom]=cost_cap_from[curidx];
+            ll realcost=potential[curidx]-potential[curfrom];
+            f.del_vertex(curfrom,curidx,tcap,realcost);
+            f.add_vertex(curidx,curfrom,tcap,-realcost);
+            curidx=curfrom;
+        }
+    }
+    if(curcap<cap){
+        return make_tuple(false,-1);
+    }else{
+        return make_tuple(true,ret);
+    }
+}
+std::tuple<bool,long long> minCostFlowNeg(ref_flow& f,size_t s,size_t t,long long cap){
+    typedef long long ll;
+    using namespace std;
+    typedef tuple<ll,ll,size_t,size_t> cost_cap_from_cur_t;
+    vector<ll> potential(f.size());
+    ll INF=LLONG_MAX;
+    ll ret = 0;
+    ll curcap=0;
+    while(curcap<cap){
+        priority_queue<cost_cap_from_cur_t,vector<cost_cap_from_cur_t>,greater<cost_cap_from_cur_t>> q;
+        vector<tuple<ll,ll,size_t>> cost_cap_from(f.size(),make_tuple(INF,0,f.size()+1));
+        cost_cap_from[s]=make_tuple(0,INF,f.size()+0);
+        auto s_to = f.get_next(s);
+        q.push(make_tuple(0,INF,f.size(),s));
+        cost_cap_from[s]=make_tuple(0,INF,f.size());
+        while(!q.empty()){
+            auto [cost,cap,from,cur]=q.top();q.pop();
+            auto v = f.get_next(cur);
+            for (auto &&i : v)
+            {
+                auto [ncost,ncap,to]=i;
+                auto [tcost,tcap,tfrom]=cost_cap_from[to];
+                ll newcost=cost+(ncost+potential[cur]-potential[to]);
+                if(newcost<tcost){
+                    cost_cap_from[to]=make_tuple(newcost,min(cap,ncap),cur);
+                    q.push(make_tuple(newcost,min(cap,ncap),cur,to));
+                }
+            }
+        }
+        for(size_t i=0;i<f.size();++i){
+            potential[i]+=get<0>(cost_cap_from[i]);
+        }
+        auto [_,tcap,tfrom]=cost_cap_from[t];
+        if(tfrom==f.size()+1||tcap<=0)break;
+        if(curcap+tcap<=cap)
+          curcap+=tcap;
+        else{
+          tcap=cap-curcap;
+          curcap=cap;
+        }
+        ret+=(potential[t])*tcap;
+        auto curidx=t;
+        while(curidx!=s){
             auto [_,__,curfrom]=cost_cap_from[curidx];
             auto [flag,___,curcost] = f.get_mincost_edge(curfrom,curidx);
             assert(flag);
